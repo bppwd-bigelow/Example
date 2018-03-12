2de61dc89c0941725c7d4255e9db0b356dd92475
matthew <m.caranci@baypath.net>
2018-03-12 13:42:34 -0400

    first commit

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -15,7 +15,7 @@ public class JavaApplication13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("hello fdjgbriebgkjebgieb");
+        System.out.print("hello sweetcheeks");
     }
     
 }
